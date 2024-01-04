fe7b9145693cf157a04c6e05c313be5dd65b16e3
Beorlor <jbdurand2000@gmail.com>
2024-01-04 03:34:35 +0100

    Pipex start

---
+++ src/pipex.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beorlor <beorlor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 03:30:06 by beorlor           #+#    #+#             */
+/*   Updated: 2024/01/04 03:30:09 by beorlor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+# include <fcntl.h>  
+# include <stdlib.h>
+
+int		open_file(char *file, int n);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	exec(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+void	exit_handler(int n_exit);
+
+#endif
---
+++ src_bonus/pipex_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beorlor <beorlor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 03:29:20 by beorlor           #+#    #+#             */
+/*   Updated: 2024/01/04 03:29:25 by beorlor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+void	exit_handler(int n_exit);
+int		open_file(char *file, int n);
+char	*my_getenv(char *name, char **env);
+char	*get_path(char *cmd, char **env);
+void	exec(char *cmd, char **env);
+void	ft_free_tab(char **tab);
+
+#endif
